97ad919ffe7bf98fd05d40e86c2a3c3ecd56659d
Melvin-Emmanuel <emmanulmelv@gmail.com>
2024-02-11 19:15:48 +0100

    updated

--- dist/Database/db.js
+++ dist/Database/db.js
@@ -7,11 +7,12 @@ const mongoose_1 = __importDefault(require("mongoose"));
 const url = "mongodb://0.0.0.0:27017/unicornkleenbackend";
 const url2 = "mongodb+srv://emmanulmelv:0n4DIP5zo5tLVmn3@cluster0.r6gtk6f.mongodb.net/";
 const Db = mongoose_1.default
-    .connect(url)
+    .connect(url2)
     .then(() => {
     console.log("connected to database");
 })
-    .catch(() => {
-    console.log("could not connect to database");
+    .catch((err) => {
+        console.log("could not connect to database"
+        +err.message);
 });
 exports.default = Db;
